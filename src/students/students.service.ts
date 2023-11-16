2d53d911bf60fec69e75751c803d2dc251d981b1
OliverP <opaucarq@uni.pe>
2023-11-16 09:19:51 -0500

    add findAll findOne remove services

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,9 +1,10 @@
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+
+import { Student } from './entities/student.entity';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
-import { Student } from './entities/student.entity';
 
 @Injectable()
 export class StudentsService {
@@ -11,31 +12,43 @@ export class StudentsService {
     @InjectRepository(Student)
     private studentsRepository: Repository<Student>,
   ) {}
-  create(createStudentDto: CreateStudentDto) {
-    const newStudent = this.studentsRepository.create(createStudentDto);
-    return this.studentsRepository.save(newStudent);
-  }
 
-  findAll() {
-    return `This action returns all students`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
-  }
-
-  async update(id: number, updateStudentDto: UpdateStudentDto) {
+  private async findStudentById(id: number): Promise<Student> {
     const foundStudent = await this.studentsRepository.findOne({
       where: { id },
     });
     if (!foundStudent) {
       throw new HttpException('User not found', HttpStatus.NOT_FOUND);
     }
+    return foundStudent;
+  }
+
+  create(createStudentDto: CreateStudentDto) {
+    const newStudent = this.studentsRepository.create(createStudentDto);
+    return this.studentsRepository.save(newStudent);
+  }
+
+  findAll() {
+    return this.studentsRepository.find({
+      select: ['id', 'firstname', 'lastname'],
+    });
+  }
+
+  async findOne(id: number) {
+    return await this.findStudentById(id);
+  }
+
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    const foundStudent = await this.findStudentById(id);
     this.studentsRepository.merge(foundStudent, updateStudentDto);
     return this.studentsRepository.save(foundStudent);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  async remove(id: number) {
+    await this.findStudentById(id);
+    const deletedUser = await this.studentsRepository.delete({ id });
+    if (deletedUser.affected) {
+      throw new HttpException('Student deleted successfully', HttpStatus.OK);
+    }
   }
 }
