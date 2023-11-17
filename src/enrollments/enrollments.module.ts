599a21b67551dbd33036aab82e21ce7121d88917
OliverP <opaucarq@uni.pe>
2023-11-17 08:16:24 -0500

    import export modules

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -5,9 +5,10 @@ import { EnrollmentsService } from './enrollments.service';
 import { EnrollmentsController } from './enrollments.controller';
 import { Enrollment } from './entities/enrollment.entity';
 import { SubjectsModule } from 'src/subjects/subjects.module';
+import { StudentsModule } from 'src/students/students.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Enrollment]), SubjectsModule],
+  imports: [TypeOrmModule.forFeature([Enrollment]), SubjectsModule, StudentsModule],
   controllers: [EnrollmentsController],
   providers: [EnrollmentsService],
 })
