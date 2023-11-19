0167a371b788f568830514afaf430a1b9fef284a
OliverP <opaucarq@uni.pe>
2023-11-18 19:07:29 -0500

    add middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import * as fs from 'fs';
+import { configureMiddlewares } from 'middlewares/middleware.config';
 import { HttpExceptionFilter } from 'http-exception.filter';
 
 async function bootstrap() {
@@ -13,26 +13,8 @@ async function bootstrap() {
     app.useGlobalFilters(new HttpExceptionFilter());
     console.log('here');
   }
+  configureMiddlewares(app);
   app.enableCors();
-
-  const logFilePath = process.env.LOG_FILE_PATH || 'response-time-log.txt';
-
-  app.use((req, res, next) => {
-    const start = Date.now();
-    res.on('finish', () => {
-      const end = Date.now();
-      const elapsedTime = end - start;
-      const logMessage = `${new Date().toISOString()} - Ruta: ${
-        req.url
-      } - Response Time: ${elapsedTime} ms\n`;
-      fs.appendFile(logFilePath, logMessage, (err) => {
-        if (err) {
-          console.error('Error al escribir en el archivo de registro:', err);
-        }
-      });
-    });
-    next();
-  });
 
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
