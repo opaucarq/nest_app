6b8dda940cd21d60700ab2717ad8229f87538d08
OliverP <opaucarq@uni.pe>
2023-11-18 19:13:02 -0500

    refactor code already in middleware

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { configureMiddlewares } from 'middlewares/middleware.config';
-import { HttpExceptionFilter } from 'http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useLogger(new Logger());
   app.useGlobalPipes(new ValidationPipe());
-  if (process.env.LOG_ERRORS === 'true') {
-    app.useGlobalFilters(new HttpExceptionFilter());
-    console.log('here');
-  }
+
   configureMiddlewares(app);
   app.enableCors();
 
