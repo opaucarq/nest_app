9de5b30715c35e6049fb8ce850302d5a467f2002
OliverP <opaucarq@uni.pe>
2023-11-20 03:31:42 -0500

    update database configuration for migrations

---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 35432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [`${__dirname}/../../db/migrations/*{.ts,.js}`],
+  migrationsTableName: 'migrations',
+}));
